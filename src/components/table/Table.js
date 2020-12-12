c29f175d9a55aa48700c1234fc53f32a64979fdf
Melber17 <63192915+Melber17@users.noreply.github.com>
2020-12-13 00:06:34 +0300

    finish base table-logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -11,10 +11,11 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options
     })
   }
+
   toHTML() {
     return createTable(20)
   }
@@ -26,20 +27,22 @@ export class Table extends ExcelComponent {
   init() {
     super.init()
 
-    const $cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select($cell)
-    this.$emit('table:select', $cell)
-     this.$on('formula:input', text => {
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
+
+    this.$on('formula:input', text => {
       this.selection.current.text(text)
     })
+
     this.$on('formula:done', () => {
       this.selection.current.focus()
     })
   }
-selectCell($cell) {
-  this.selection.selet($cell)
-  this.$emit('table:select', $cell)
-}
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event)
@@ -71,8 +74,11 @@ selectCell($cell) {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
-      this.$emit('table:select', $next)
+      this.selectCell($next)
     }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
